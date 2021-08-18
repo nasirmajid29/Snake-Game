5c14455ee621e36523b762693a148d3de31fa64b
nasir <nm219@ic.ac.uk>
2021-08-18 16:26:57 +0100

    added default position of snake, fruit and a score displaying function

--- main.py
+++ main.py
@@ -19,3 +19,34 @@ pygame.init()
 pygame.display.set_caption('Snake Game')
 game_window = pygame.display.set_mode((window_x, window_y))
 fps = pygame.time.Clock()
+
+# default snake position
+snake_position = [100, 50]
+
+# defining first 4 blocks of snake body
+snake_body = [[100, 50], [90, 50], [80, 50], [70, 50]]
+
+# fruit position
+fruit_position = [random.randrange(1, (window_x // 10)) * 10, random.randrange(1, (window_y // 10)) * 10]
+fruit_spawn = True
+
+# setting default snake direction towards right
+direction = 'RIGHT'
+change_to = direction
+
+score = 0
+
+
+# displaying Score function
+def show_score(choice, color, font, size):
+    # creating font object score_font
+    score_font = pygame.font.SysFont(font, size)
+
+    # create the display surface object
+    score_surface = score_font.render('Score : ' + str(score), True, color)
+
+    # create a rectangular object for the text surface object
+    score_rect = score_surface.get_rect()
+
+    # displaying text
+    game_window.blit(score_surface, score_rect)
