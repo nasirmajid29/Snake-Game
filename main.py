682e35d20f2b15540ff21a980d413b9bdf1ae047
nasir <nm219@ic.ac.uk>
2021-08-18 16:31:08 +0100

    added game over displaying function

--- main.py
+++ main.py
@@ -50,3 +50,23 @@ def show_score(choice, color, font, size):
 
     # displaying text
     game_window.blit(score_surface, score_rect)
+
+
+# game over function
+def game_over():
+    my_font = pygame.font.SysFont('times new roman', 50)
+
+    # creating a text surface
+    game_over_surface = my_font.render('Your Score is : ' + str(score), True, red)
+
+    # create a rectangular object for the text surface object
+    game_over_rect = game_over_surface.get_rect()
+
+    # setting position of the text
+    game_over_rect.midtop = (window_x / 2, window_y / 4)
+    game_window.blit(game_over_surface, game_over_rect)
+    pygame.display.flip()
+
+    time.sleep(2)
+    pygame.quit()
+    quit()
